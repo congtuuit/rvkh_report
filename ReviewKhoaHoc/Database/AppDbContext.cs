1fdb6e0db759ec4e427cfce78dd0bc71e50627a1
tu.van.home <congtuuit@gmail.com>
2025-10-08 09:00:25 +0700

    init

---
+++ ReviewKhoaHoc/Database/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewKhoaHoc.Entities;
+
+namespace ReviewKhoaHoc.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CourseViewAudit> CourseViewAudits => Set<CourseViewAudit>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CourseViewAudit>(entity =>
+            {
+                entity.ToTable("course_view_audit");
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.CourseName).HasMaxLength(255);
+                entity.Property(x => x.CourseType).HasMaxLength(100);
+                entity.HasIndex(x => x.ViewedAt);
+            });
+        }
+    }
+
+}
---
+++ ReviewKhoaHoc/Entities/CourseViewAudit.cs
@@ -0,0 +1,14 @@
+﻿namespace ReviewKhoaHoc.Entities
+{
+    public class CourseViewAudit
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid CourseId { get; set; }
+        public string CourseName { get; set; } = string.Empty;
+        public string CourseType { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string CourseLink { get; set; } = string.Empty;
+        public int ViewCount { get; set; } = 1;
+        public DateTime ViewedAt { get; set; } = DateTime.UtcNow;
+    }
+}
